903c27cd7faf693b53cffe0f856329741cce91a1
Jayzn <jayznx@icloud.com>
2026-01-10 04:32:16 +0000

    Add C files

---
+++ if_01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+
+
+        for (int j = 0; j < 3; j++) {
+
+            printf("[%d, %d] ", i, j);
+
+
+        }
+        printf("\n"); // new line after each row
+
+    }
+
+    return 0;
+
+}
+
+
+	
